b3e1e2fad4b321a640bff6235a05a94599a973d8
Leon <letconex@gmail.com>
2020-12-26 11:08:58 +0200

    Update dexonline.py
    Added license, changed json to text as a response type. Added usage examples.

--- Python Scripts/dexonline.py
+++ Python Scripts/dexonline.py
@@ -1,10 +1,16 @@
+# -*- coding: utf-8 -*-
 import requests, json, sys
-# Usage: python dexonlineAPI.py <word> <responsetype>
+# License: GPL
+# Usage: python dexonlineAPI.py <word> <responsetype>.
+# The argument order is binding, the second argument is not imperative, if missing will default to "html".
+# Example: python dexonlineAPI.py "casă" text
+# Example: python dexonlineAPI.py "casă" html
+# Example without second optional argument: python dexonlineAPI.py "casă"
 word = sys.argv[1]
-responsetype = sys.argv[2] # json or html formatted
+responsetype = sys.argv[2] # plain text or html formatted
 
 try: # if responsetype is not entered, response type defaults to html
-    responsetype = sys.argv[2] # "json" or "html"
+    responsetype = sys.argv[2] # "text" or "html"
 except:
     # print('No response type entered, will default to "html."\r\n')
     responsetype = 'html'
@@ -20,10 +26,10 @@ else:
     for defs in responsetext['definitions']:
       print(defs['htmlRep'])
     print('<br>Results fetched successfully courtesy of <a href="http://www.dexonline.ro">www.dexonline.ro</a>')
-  elif responsetype == 'json':
+  elif responsetype == 'text':
     for defs in responsetext['definitions']:
       print(defs['internalRep'])
     print('\r\nResults fetched courtesy of dexonline.ro')
   else:
-    print('Second argument must be "html" or "json"')
+    print('Second argument must be "html" or "text"')
 sys.exit('A big thanks to all contributors to the dexonline.ro project!')
